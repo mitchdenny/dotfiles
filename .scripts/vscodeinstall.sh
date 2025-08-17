b706f7e9db1faeb668fec3eda53faf390155afb7
Mitch Denny <mitch@mitchdenny.com>
2025-08-17 11:30:43 +1000

    Update scripts.

--- .scripts/vscodeinstall.sh
+++ .scripts/vscodeinstall.sh
@@ -2,3 +2,4 @@
 sudo pacman -S visual-studio-code-bin
 code --install-extension ms-dotnettools.csdevkit
 code --install-extension ms-azuretools.vscode-containers
+code --install-extension ms-vscode-remote.remote-containers
